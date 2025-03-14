5346e02595ea4d2b2eddcc333ae66d6a9b88f02b
Александр <alex_a89@mail.ru>
2025-03-14 21:26:36 +0300

    -исправление недочётов

--- pages/start_page.py
+++ pages/start_page.py
@@ -16,20 +16,19 @@ class StartPage(Base):
     url = 'https://www.onlinetrade.ru/'  # url тестируемого сайта
 
     # Locators
-    accept_cookie = '//a[contains(text(), "Принять")]'                                              # локатор кнопки "Принять" во всплывающем окне
-    first_slider_slide7 = '//span[@aria-label="Go to slide 7"]'                                     # локатор 5 слайда в первом слайдере
-    grill = '//a[@href="/catalogue/aerogrili-c1145/kitfort/aerogril_kitfort_kt_2263-4591425.html"]' # локатор ссылки на товар
+    accept_cookie = '//a[contains(text(), "Принять")]'  # локатор кнопки "Принять" во всплывающем окне
+    first_slider_slide6 = '//span[@aria-label="Go to slide 6"]'  # локатор 5 слайда в первом слайдере
+    grill = '//a[@href="/catalogue/aerogrili-c1145/kitfort/aerogril_kitfort_kt_2263-4591425.html"]'  # локатор ссылки на товар
 
     # Getters
     def get_accept_cookie(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.accept_cookie)))
 
-    def get_first_slider_slide7(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.first_slider_slide7)))
+    def get_first_slider_slide6(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.first_slider_slide6)))
 
     def get_grill(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.grill)))
-
 
     # Actions
     def click_accept_cookie(self):
@@ -37,23 +36,20 @@ class StartPage(Base):
         print("Нажимаем на кнопку 'Принять куки'")
 
     def open_grill(self):
-        self.get_first_slider_slide7().click()
+        self.get_first_slider_slide6().click()
         time.sleep(2)
         print("В первом слайдере выбираем 7 слайд")
         self.get_grill().click()
         time.sleep(2)
         print('Открываем страницу товара "Гриль"')
 
-
     # Methods
     def select_product_from_slider(self):
         with allure.step("Select product from slider"):
-            Logger.add_start_step(method='select_product_from_slider')          # начало логгирования
-            self.driver.get(self.url)                                           # открываем наш url
-            self.driver.maximize_window()                                       # раскрываем страницу на весь экран
-            self.get_current_url()                                              # отображаем текущий url адрес
-            self.click_accept_cookie()                                          # принимаем куки
-            self.open_grill()                                                   # открываем страницу гриля из 6 слайда в первом слайдере
-            Logger.add_end_step(url=self.driver.current_url, method='select_product_from_slider')   # конец логгирования
-
-
+            Logger.add_start_step(method='select_product_from_slider')  # начало логгирования
+            self.driver.get(self.url)  # открываем наш url
+            self.driver.maximize_window()  # раскрываем страницу на весь экран
+            self.get_current_url()  # отображаем текущий url адрес
+            self.click_accept_cookie()  # принимаем куки
+            self.open_grill()   # открываем страницу гриля из 6 слайда в первом слайдере
+            Logger.add_end_step(url=self.driver.current_url, method='select_product_from_slider')  # конец логгирования
